122c91e5bcec09a2e598451267ba3f7cab012246
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-02-28 23:17:47 -0300

    Removing targets from commit

--- semeru-ejb-maven-ejb/target/classes/import.sql
+++
@@ -1,2 +0,0 @@
-create table Registrant (id bigint not null, email varchar(255) not null, name varchar(25) not null, phone_number varchar(12) not null, primary key (id));
-insert into Registrant(id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
